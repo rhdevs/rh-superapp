56cdc31e2e0d04a90ba398bbf2b576003931fce6
yidouweng <>
2023-02-18 20:10:16 +0800

    created yearbook events page

---
+++ pages/yearbook_events.tsx
@@ -0,0 +1,23 @@
+import PageSectionComponent from '@/components/sample-rhdevs-website/PageSection'
+import { InformationMainContainer } from '@/styles/sample-rhdevs-website/GlobalStyledComponents'
+import { events } from '@/texts/sample-rhdevs-website/descriptions/events'
+import rhdevsWebsiteWrapper from './sample-rhdevs-website/_rhdevsWebsiteWrapper'
+
+function YearbookEvents() {
+  return (
+    <InformationMainContainer>
+      {events.map((event, index) => (
+        <PageSectionComponent
+          key={index}
+          title={event.title}
+          description={event.description}
+          imgPosition={index % 2 === 0 ? 'left' : 'right'}
+          imageSrc={event.image}
+          events
+        />
+      ))}
+    </InformationMainContainer>
+  )
+}
+
+export default YearbookEvents
