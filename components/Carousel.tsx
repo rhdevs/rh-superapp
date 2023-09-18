cf00edfdc014b48db3fa7d79cb62d570f0ec93fb
Andre Chua <110965035+andrechuakj@users.noreply.github.com>
2023-09-18 16:56:19 +0800

    Add image carousel

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -1,29 +1,60 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Slider from 'react-styled-carousel'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
 import Image from 'next/image'
 
 const responsive = [
+  { breakPoint: 640, cardsToShow: 3 },
   { breakPoint: 1280, cardsToShow: 4 }, // this will be applied if screen size is greater than 1280px. cardsToShow will become 4.
-  { breakPoint: 840, cardsToShow: 2 },
 ]
 
+const ImageContainer = styled.div<{ $height?: number }>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 10px;
+  height: ${(props) => props.$height?.toString() + 'px' || 'auto'};
+  width: 300px;
+  position: relative;
+`
+
 const Carousel = ({ imgArr }) => {
+  const [maxImageWidth, setMaxImageWidth] = useState(0)
+  const [imageHeight, setImageHeight] = useState(0)
+
+  const imageStyle = {
+    padding: '10px',
+    maxWidth: `${maxImageWidth}px`,
+  }
+
+  useEffect(() => {
+    const updateImageDimensions = () => {
+      setMaxImageWidth(window.innerWidth / 4)
+      setImageHeight(window.innerWidth / 6)
+    }
+
+    updateImageDimensions()
+
+    window.addEventListener('resize', updateImageDimensions)
+
+    return () => {
+      window.removeEventListener('resize', updateImageDimensions)
+    }
+  }, [])
+
   return (
-    <Slider reponsive={responsive} showDots={true}>
+    <Slider responsive={responsive} showDots={true} autoSlide={3000}>
       {imgArr &&
         imgArr.map((item) => (
-          <Image
-            key={item.id}
-            src={item.src}
-            width={window.innerWidth / 4}
-            height={window.innerWidth / 6}
-            alt=""
-          />
+          <ImageContainer key={item.id} $height={imageHeight}>
+            <Image src={item.src} layout="fill" objectFit="contain" alt="" style={imageStyle} />
+          </ImageContainer>
         ))}
     </Slider>
   )
 }
+
 Carousel.propTypes = {
   imgArr: PropTypes.array.isRequired,
 }
