f4e7f95265e0fe6b48b1ef54506369c71528ef54
Kieron Seven Lee <kieronsevenlee@u.nus.edu>
2023-11-06 12:53:14 +0800

    Import calendar package

--- pages/calendar.tsx
+++ pages/calendar.tsx
@@ -1,15 +1,27 @@
 import styled from 'styled-components'
 import { Events } from '@/components/LandingItems'
+import React, { useState } from 'react'
+import Calendar from 'react-calendar'
 
 const PageWrapper = styled.div`
-    display: flex
-    flex-direction: row
+  display: flex;
+  flex-direction: column;
 `
 
-const Calendar = () => (
-  <PageWrapper>
-    <h1>WheeWhoo!</h1>
-    <Events />
-  </PageWrapper>
-)
-export default Calendar
+export default function MyApp() {
+  const [value, setValue] = useState(new Date())
+
+  function onChange(nextValue) {
+    setValue(nextValue)
+  }
+
+  return (
+    <div>
+      <PageWrapper>
+        <h1>WheeWhoo!</h1>
+        <Calendar onChange={onChange} value={value} />
+        {/* <Events /> */}
+      </PageWrapper>
+    </div>
+  )
+}
